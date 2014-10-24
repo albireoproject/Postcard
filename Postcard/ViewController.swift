0e74c11ce05081a5a96dec62410b4b2893a5218a
Hongsun Yoon <albireo@Albireos-DevBook.local>
2014-10-24 22:50:43 +0900

    challenge commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -12,9 +12,12 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,11 +35,21 @@ class ViewController: UIViewController {
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
+
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
         
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
+    
         
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+
+        
     }
 
 }
